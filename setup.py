abf07dc0cda0eab9e674e07c3f4ec8c38b65779b
wu.zheng <zhengwu314@163.com>
2017-12-15 10:14:27 +0800

    update setup

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@
 from distutils.core import setup
 
 setup(name = 'Distutils',
-    version = '1.0',
+    version = '0.1.0',
     description = 'Fool Nature Language toolkit ',
     author = 'wu.zheng',
     author_email = 'rocky.zheng314@gmail.com',
-    url = '',
+    url = 'https://github.com/rockyzhengwu/FoolNLTK',
     packages = ['fool'],
     package_dir = {"fool": "fool"},
     data_files = [("fool", ["data/maps.pkl", "data/ner_pos.pb", "data/seg.pb", "data/pos.pb"])]
